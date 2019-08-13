5a9be0fb35b9c4ff28f49c9688e580e6fccfe5b6
Jacob <jacobdeansd@gmail.com>
2019-08-12 20:18:25 -0700

    Updated team selector

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,53 +1,62 @@
-import React, { Component } from 'react';
-import CardList from '../Components/CardList.js';
-import SearchBox from '../Components/SearchBox.js';
-import Scroll from '../Components/Scroll.js';
-import DropdownTeamSelection from '../Components/DropdownTeamSelection';
-import './App.css';
-
+import React, { Component } from "react";
+import CardList from "../Components/CardList.js";
+import SearchBox from "../Components/SearchBox.js";
+import Scroll from "../Components/Scroll.js";
+import DropdownTeamSelection from "../Components/DropdownTeamSelection";
+import "./App.css";
 
 class App extends Component {
-    constructor(props) {
-        super()
-        this.state = {
-            team: [],
-            searchfield: '',
-            selected: 'lac'
-        }
-    }
+  constructor(props) {
+    super();
+    this.state = {
+      team: [],
+      searchfield: ""
+    };
+  }
 
+  componentDidMount() {
+    this.doTeamFetch("lac");
+  }
 
-    componentDidMount() {
-        fetch(`https://api.sportsdata.io/v3/nfl/scores/json/Players/${this.state.selected}?key=680357673cd14ef28c1bd63359aa9c48`)
-        .then(response => response.json())
-        .then(players => this.setState({ team: players}));
-    }
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+  };
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value})   
-    }
+  doTeamFetch(team) {
+    fetch(
+      `https://api.sportsdata.io/v3/nfl/scores/json/Players/${team}?key=680357673cd14ef28c1bd63359aa9c48`
+    )
+      .then(response => response.json())
+      .then(players => this.setState({ team: players }));
+  }
 
-    render() {
-        const { team, searchfield } = this.state;
-        const filteredPlayers = team.filter(player => {
-            return player.Name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-        return !team.length ? 
-            <div className='tc'>
-                <h1 className='f1'>NFL Rosters</h1>
-                <DropdownTeamSelection />
-            </div> : 
-        (
-            <div className='tc'>
-                <h1 className='f1'>NFL Rosters</h1>
-                <DropdownTeamSelection />
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                    <CardList team={filteredPlayers}/>
-                </Scroll>
-            </div>
-        );
-    }
+  onDropDownChange(team) {
+    this.doTeamFetch(team);
+  }
+
+  render() {
+    const { team, searchfield } = this.state;
+    const filteredPlayers = team.filter(player => {
+      return player.Name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    return !team.length ? (
+      <div className="tc">
+        <h1 className="f1">NFL Rosters</h1>
+        <DropdownTeamSelection />
+      </div>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">NFL Rosters</h1>
+        <DropdownTeamSelection
+          onDropDownChange={team => this.onDropDownChange(team)}
+        />
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList team={filteredPlayers} />
+        </Scroll>
+      </div>
+    );
+  }
 }
 
 export default App;
