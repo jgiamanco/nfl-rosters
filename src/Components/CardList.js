4621cee743a5b4592bdaa98db0116ad7f0abee81
Jacob <jacobdeansd@gmail.com>
2019-08-13 15:19:00 -0700

    adjustments

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -4,7 +4,7 @@ import Card from './Card.js';
 
 const CardList = ({team}) => {
     return (
-        <div>
+        <div className="card-container">
             {
                 team.map((player, i) => {
                     return (
