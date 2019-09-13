65031d00a4a6ae5667414c6727ab6e6863b2a99c
Jacob <jacobdeansd@gmail.com>
2019-09-13 13:06:46 -0700

    moved build for pages

---
+++ precache-manifest.db4a70cfad2d7806144a0cdfbf49d52b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092cacfe236c7edcfc653714332b36f0",
+    "url": "/nfl-rosters/index.html"
+  },
+  {
+    "revision": "75fe3e910c6a1a8306a6",
+    "url": "/nfl-rosters/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "8b2bac3977926d42bb88",
+    "url": "/nfl-rosters/static/css/main.e3fdb487.chunk.css"
+  },
+  {
+    "revision": "75fe3e910c6a1a8306a6",
+    "url": "/nfl-rosters/static/js/2.2d781f7c.chunk.js"
+  },
+  {
+    "revision": "8b2bac3977926d42bb88",
+    "url": "/nfl-rosters/static/js/main.ed70152a.chunk.js"
+  },
+  {
+    "revision": "9daa82add3f67f7cb0e0",
+    "url": "/nfl-rosters/static/js/runtime~main.26d959de.js"
+  }
+]);
