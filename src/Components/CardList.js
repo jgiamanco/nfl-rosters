34b87e64a95d2c05736b0b461dca0a0eb93715e3
Jacob <jacobdeansd@gmail.com>
2020-05-17 12:38:04 -0700

    experience string

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -13,7 +13,7 @@ const CardList = ({team}) => {
                             id={team[i].PhotoUrl !== "https://s3-us-west-2.amazonaws.com/static.fantasydata.com/headshots/nfl/low-res/0.png" ? team[i].PlayerID : 0} 
                             name={team[i].Name} 
                             position={team[i].Position}
-                            years={team[i].Experience} 
+                            years={team[i].ExperienceString} 
                             number={team[i].Number}
                         />
                     );        
